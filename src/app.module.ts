c755626d5471ceb6a2df2678051fcf026d8dc92f
Shin <penspulse@gmail.com>
2026-07-15 00:57:11 +0800

    feat: add basic db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [AppController],
-  imports: [],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        autoLoadEntities: true,
+        database: configService.get<string>('DB_DATABASE'),
+        host: configService.get<string>('DB_HOST'),
+        logging: true,
+        password: configService.get<string>('DB_PASSWORD'),
+        port: configService.get<number>('DB_PORT'),
+        synchronize: true,
+        type: 'postgres',
+        username: configService.get<string>('DB_USERNAME'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
