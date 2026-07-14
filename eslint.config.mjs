884fd5f8c6e07fd6deffb2747bd1c148605050ca
Shin <penspulse@gmail.com>
2026-07-15 00:10:08 +0800

    chore: add initial nodejs project settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import perfectionist from 'eslint-plugin-perfectionist';
 
 export default tseslint.config(
   {
@@ -11,6 +12,7 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  perfectionist.configs['recommended-natural'],
   {
     languageOptions: {
       globals: {
